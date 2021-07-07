c58d4126a999913fbbaa6c1a22807983c7c70c2d
holea <holea@LAPTOP-GAA1GLSI>
2021-07-07 15:12:27 +0530

    Main method is updated

--- src/main/java/demoProject/HelloWorld.java
+++ src/main/java/demoProject/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world...!!!");
+		System.out.println("Hello");
+		System.out.println("GitHub");
+		
 	}
 
 }
